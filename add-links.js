8832d7af5d1421ade63a1477aa4b11de209b4e6f
Dmitri Suvorov <suvjunmd@gmail.com>
2022-10-20 00:16:36 +0300

    Finish format-doc

--- add-links.js
+++ add-links.js
@@ -1,31 +1,31 @@
 // add capitol links
-document.querySelectorAll('h2').forEach(node => {
+document.querySelectorAll("h2").forEach((node) => {
   const content = node.innerHTML;
   const artNumber = content.match(/\d+/);
   node.innerHTML = `<a id="cap${artNumber}">${content}</a>`;
 });
 
 // add article links
-document.querySelectorAll('h3').forEach(node => {
+document.querySelectorAll("h3").forEach((node) => {
   const content = node.innerHTML;
   const artNumber = content.match(/\d+/);
   node.innerHTML = `<a id="art${artNumber}">${content}</a>`;
 });
 
 // add navigation
-const nav = document.createElement('nav');
-const h3 = document.createElement('h3');
-h3.innerHTML = 'Conţinut';
+const nav = document.createElement("nav");
+const h3 = document.createElement("h3");
+h3.innerHTML = "Conţinut";
 nav.appendChild(h3);
-const ul = document.createElement('ul');
+const ul = document.createElement("ul");
 nav.appendChild(ul);
 
-document.querySelectorAll('a').forEach(node => {
+document.querySelectorAll("a").forEach((node) => {
   const content = node.innerHTML;
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.innerHTML = `<a href="#${node.id}">${content}</a>`;
   ul.appendChild(li);
 });
 
-const header = document.getElementsByTagName('header')[0];
+const header = document.getElementsByTagName("header")[0];
 header.after(nav);
--- format-doc.js
+++ format-doc.js
@@ -105,76 +105,150 @@ document.querySelectorAll("h2").forEach((h2) => {
 });
 
 // add footer
-const allParagraphs = document.querySelectorAll('main > p');
+const allParagraphs = document.querySelectorAll("main > p");
 const footerFirstLine = allParagraphs[allParagraphs.length - 2];
-footerFirstLine.innerHTML = footerFirstLine.innerHTML.replaceAll("&nbsp;", " ");
-const formattedFooterFirstLine = footerFirstLine.innerText.replace('ПРЕДСЕДАТЕЛЬ ПАРЛАМЕНТА', 'Председатель парламента')
+footerFirstLine.innerHTML = footerFirstLine.innerHTML
+  .replaceAll("&nbsp;", " ")
+  .trim();
+const formattedFooterFirstLine = footerFirstLine.innerText.replace(
+  "ПРЕДСЕДАТЕЛЬ ПАРЛАМЕНТА",
+  "Председатель парламента"
+);
 const footerSecondLine = allParagraphs[allParagraphs.length - 1];
-footerSecondLine.innerHTML = footerSecondLine.innerHTML.replaceAll("&nbsp;", " ");
+footerSecondLine.innerHTML = footerSecondLine.innerHTML
+  .replaceAll("&nbsp;", " ")
+  .trim();
 
-const footer = document.createElement('footer');
-const pFooterAuthor = document.createElement('p');
+const footer = document.createElement("footer");
+const pFooterAuthor = document.createElement("p");
 pFooterAuthor.innerHTML = `<strong>${formattedFooterFirstLine}</strong>`;
-const pFooterDate = document.createElement('p');
+const pFooterDate = document.createElement("p");
 pFooterDate.innerHTML = `<strong>${footerSecondLine.innerText}</strong>`;
 footer.append(pFooterAuthor, pFooterDate);
 document.body.append(footer);
 footerFirstLine.remove();
 footerSecondLine.remove();
 
-// change p with sup to h3
-document.querySelectorAll('main > p > strong > span > span > span > sup').forEach(p => {
-  const firstSpan = p.parentElement;
-  const secondSpan = p.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
-  secondSpan.innerHTML = secondSpan.innerHTML.replace("&nbsp;", "");
-  const parent = secondSpan.parentElement;
-  parent.outerHTML = `<h3>${firstSpan.innerHTML} ${secondSpan.innerText}</h3>`;
-});
+// change article titles with sup from p to h3
+document
+  .querySelectorAll("main > p > strong:first-child > span > span > span > sup")
+  .forEach((sup) => {
+    const firstSpan = sup.parentElement;
+    const secondSpan =
+      firstSpan.parentElement.parentElement.parentElement.nextElementSibling;
+    const secondSpanChild = secondSpan.firstElementChild.firstElementChild;
+    secondSpanChild.innerHTML = secondSpanChild.innerHTML
+      .replace("&nbsp;", " ")
+      .trim();
+    const parent = secondSpan.parentElement;
+    parent.outerHTML = `<h3>${firstSpan.innerHTML}${secondSpanChild.innerHTML}</h3>`;
+  });
 
-// change p to h3
-document.querySelectorAll('main > p > strong + span + em').forEach(em => {
-  const parent = em.parentElement;
-  const emChild = em.firstElementChild.firstElementChild.firstElementChild;
-  const span = em.previousElementSibling;
-  const spanChild = span.firstElementChild.firstElementChild;
-  spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ");
-  const strong = span.previousElementSibling;
-  const strongChild = strong.firstElementChild.firstElementChild.firstElementChild;
-  strongChild.innerHTML = strongChild.innerHTML.replaceAll("&nbsp;", " ");
-  parent.outerHTML = `<h3>${strongChild.innerHTML}${spanChild.innerHTML}${emChild.innerHTML}</h3>`;
-});
+// change expired article titles from p to h3
+document
+  .querySelectorAll("main > p > strong:first-child + span + em:last-child")
+  .forEach((em) => {
+    const parent = em.parentElement;
+    const emChild = em.firstElementChild.firstElementChild.firstElementChild;
+    const span = em.previousElementSibling;
+    const spanChild = span.firstElementChild.firstElementChild;
+    spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const strong = span.previousElementSibling;
+    const strongChild =
+      strong.firstElementChild.firstElementChild.firstElementChild;
+    strongChild.innerHTML = strongChild.innerHTML
+      .replaceAll("&nbsp;", " ")
+      .trim();
+    parent.outerHTML = `<h3>${strongChild.innerHTML} ${spanChild.innerHTML} ${emChild.innerHTML}</h3>`;
+  });
 
-document.querySelectorAll('main > p > strong + span').forEach(span => {
-  const parent = span.parentElement;
-  const spanChild = span.firstElementChild.firstElementChild;
-  spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ");
-  const strong = span.previousElementSibling;
-  const strongChild = strong.firstElementChild.firstElementChild.firstElementChild;
-  strongChild.innerHTML = strongChild.innerHTML.replaceAll("&nbsp;", " ");
-  parent.outerHTML = `<h3>${strongChild.innerHTML}${spanChild.innerHTML}</h3>`;
-});
+// change regular article title from p to h3
+document
+  .querySelectorAll("main > p > strong:first-child + span:last-child")
+  .forEach((span) => {
+    const parent = span.parentElement;
+    const spanChild = span.firstElementChild.firstElementChild;
+    spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const strong = span.previousElementSibling;
+    const strongChild =
+      strong.firstElementChild.firstElementChild.firstElementChild;
+    strongChild.innerHTML = strongChild.innerHTML
+      .replaceAll("&nbsp;", " ")
+      .trim();
+    parent.outerHTML = `<h3>${strongChild.innerHTML} ${spanChild.innerHTML}</h3>`;
+  });
 
-document.querySelectorAll('main > p > strong:only-child').forEach(strong => {
+// change article title without name from p to h3
+document.querySelectorAll("main > p > strong:only-child").forEach((strong) => {
   const parent = strong.parentElement;
-  const strongChild = strong.firstElementChild.firstElementChild.firstElementChild;
-  strongChild.innerHTML = strongChild.innerHTML.replaceAll("&nbsp;", " ");
+  const strongChild =
+    strong.firstElementChild.firstElementChild.firstElementChild;
+  strongChild.innerHTML = strongChild.innerHTML
+    .replaceAll("&nbsp;", " ")
+    .trim();
   parent.outerHTML = `<h3>${strongChild.innerHTML}</h3>`;
 });
 
 // change p with strange format to h3
-document.querySelectorAll('main > p > span > span > span > strong').forEach(strong => {
-  const parent = strong.parentElement.parentElement.parentElement.parentElement;
-  parent.innerHTML = parent.innerHTML.replaceAll("&nbsp;", " ")
-  parent.outerHTML = `<h3>${parent.innerText}</h3>`;
+document
+  .querySelectorAll("main > p > span:only-child > span > span > strong")
+  .forEach((strong) => {
+    const parent =
+      strong.parentElement.parentElement.parentElement.parentElement;
+    parent.innerHTML = parent.innerHTML.replaceAll("&nbsp;", " ").trim();
+    parent.outerHTML = `<h3>${parent.innerText}</h3>`;
+  });
+
+// format expired paragraphs
+document
+  .querySelectorAll("main > p > span:first-child + em:last-child")
+  .forEach((em) => {
+    const emChild = em.firstChild.firstChild.firstChild;
+    emChild.innerHTML = emChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const spanChild =
+      em.previousElementSibling.firstElementChild.firstElementChild;
+    spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const parent = em.parentElement;
+    parent.outerHTML = `<p>${spanChild.innerHTML} ${emChild.innerHTML}</p>`;
+  });
+
+// format regular paragraphs
+document.querySelectorAll("main > p > span:only-child").forEach((span) => {
+  const spanChild = span.querySelector("span > span > span");
+  spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+  const parent = span.parentElement;
+  parent.outerHTML = `<p>${spanChild.innerHTML}</p>`;
 });
 
-// format expired article points
-document.querySelectorAll('main > p > span  + em').forEach(em => {
-  const emChild = em.firstChild.firstChild.firstChild;
-  emChild.innerHTML = emChild.innerHTML.replaceAll("&nbsp;", " ");
-  const spanChild = em.previousElementSibling.firstElementChild.firstElementChild;
-  spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ");
-  const parent = em.parentElement;
-  parent.outerHTML = `<p>${spanChild.innerHTML}${emChild.innerHTML}</p>`;
-});
+// format paragraphs with br in front
+document
+  .querySelectorAll("main > p > br:first-child + span:last-child")
+  .forEach((span) => {
+    const spanChild = span.querySelector("span > span > span");
+    spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const parent = span.parentElement;
+    parent.outerHTML = `<p>${spanChild.innerHTML}</p>`;
+  });
 
+// format paragraphs with em in front
+document
+  .querySelectorAll("main > p > em:first-child + span:last-child")
+  .forEach((span) => {
+    const spanChild = span.querySelector("span > span > span");
+    spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const emChild =
+      span.previousElementSibling.querySelector("span > span > span");
+    emChild.innerHTML = emChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const parent = span.parentElement;
+    parent.outerHTML = `<p><em>${emChild.innerHTML}</em> ${spanChild.innerHTML}</p>`;
+  });
+
+// format modified paragraphs
+document
+  .querySelectorAll('main > p > em:only-child > span[style="color:blue;"]')
+  .forEach((span) => {
+    const spanChild = span.querySelector("span > span > span");
+    spanChild.innerHTML = spanChild.innerHTML.replaceAll("&nbsp;", " ").trim();
+    const parent = span.parentElement.parentElement;
+    parent.outerHTML = `<p class="modified">${spanChild.innerHTML}</p>`;
+  });
